8c5f03e341d3e46786bfbc79674935669d0e0d6d
A-Kolganov <tema.crow@gmail.com>
2019-05-23 19:23:05 +0300

    check module build

--- src/js/view/content-module.js
+++ src/js/view/content-module.js
@@ -5,9 +5,13 @@ function showContentModule(e) {
     e.preventDefault();
     
     const content = document.querySelector('.content__inner');
-    const innerContent = document.querySelector('.content__content-info__card');
-    content.removeChild(content.firstChild);
-    content.insertAdjacentHTML("afterbegin", contentModule(moduleObj))
+    console.log(content.children.length)
+    if (content.children.length !== 0){
+        while (content.hasChildNodes()) {
+            content.removeChild(content.firstChild);
+         }
+    }
+    moduleObj.forEach(i=>content.insertAdjacentHTML("beforeend", contentModule(i)));
 }
 
 export {showContentModule};
